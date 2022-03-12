dfc9ec4cd213b290ce1239490d33984327a15482
munguriek <benjaminmunguriek@gmail.com>
2022-03-12 14:09:56 +0300

    Added the full CRUD functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogHomeView(ListView):
@@ -10,3 +12,18 @@ class BlogDetailView(DetailView):
     model = Post 
     template_name = 'post_detail.html'
     context_object_name = 'post'
+    
+class BlogCreateView(CreateView):
+    model = Post 
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'author']
+    
+class BlogUpdateView(UpdateView):
+    model = Post 
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model = Post 
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
